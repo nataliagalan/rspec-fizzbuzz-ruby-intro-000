0d55821f2bd0a3ff360dbeded47e36404f78de2b
Natalia Galan <n8galan@gmail.com>
2020-06-08 00:17:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,21 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  return "Fizz" if fizz_3(number)
-  return "Buzz" if fizz_5(number)
-  return "FizzBuzz" if fizz_15(number)
+  return "FizzBuzz" if is_divisible_by_fifteen?(number)
+  return "Buzz" if is_divisible_by_five?(number)
+  return "Fizz" if is_divisible_by_three?(number)
   number
 end
 
-def fizz_3?(number)
+def is_divisible_by_three?(number)
   is_divisible_by(number, 3)
 end
 
-def fizz_5?(number)
+def is_divisible_by_five?(number)
   is_divisible_by(number, 5)
 end
 
-def fizz_15?(number)
+def is_divisible_by_fifteen?(number)
   is_divisible_by(number, 15)
 end
 
