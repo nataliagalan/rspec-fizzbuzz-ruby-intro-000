4de85f1e8059db0c8b7170ad1cf59798392bc433
Natalia Galan <n8galan@gmail.com>
2020-06-08 00:14:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
+  return "Fizz" if fizz_3(number)
   return "FizzBuzz" if fizz_15(number)
   return "Buzz" if fizz_5(number)
-  return "Fizz" if fizz_3(number)
+
   number
 end
-
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
-
-    expect(fizz_3).to eq("Fizz")
-  end
-  it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz(5)
-
-    expect(fizz_5).to eq("Buzz")
-  end
 
 def is_divisible_by_three?(number)
   is_divisible_by(number, 3)
