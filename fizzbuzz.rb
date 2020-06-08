859e67bfc79271a59b39ab8b8645da3df925194a
Natalia Galan <n8galan@gmail.com>
2020-06-08 00:07:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  return "FizzBuzz" if fizzbuzz(15)
+  return "FizzBuzz" if fizzbuzz(number)
   return "Buzz" if buzz(number)
   return "Fizz" if fizzbuzz(number)
   return nil if is_not_divisible_by_three_or_five?(number)
